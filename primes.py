10620b428a1a0c2e55092f159a94b5f3c636ba56
Matt Stanbrell <mattstanbrell@gmail.com>
2022-10-13 16:20:57 +0100

    Raises ValueError for <= 0 values

--- primes.py
+++ primes.py
@@ -1,6 +1,19 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("Input value must be positive.")
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        if is_prime(num):
+            list.append(num)
+        num += 1
+
     return list
+
+
+def is_prime(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
